f5e9a0edd7af0d2918f582e19abbcb6bdeec2759
Chen Lu <chenlu.design@gmail.com>
2013-07-30 17:24:20 +0100

    initial commit

---
+++ data/properties.js
@@ -0,0 +1,52 @@
+﻿{
+    "property": [
+        {
+        	"id": 29862117,
+            "transaction_type": 1,
+            "property_placement": "Third Floor",
+            "property_type": "Flat",
+            "latitude": 51.504575,
+            "longitude": -0.124594,
+            "address": "Whitehall Court, St James's, London",
+            "postcode": "SW1A",
+            "bedrooms": 1,
+            "price": 1400000,
+            "pricetype": "Fixed",
+            "summary": "An impressive pied-a-terre on the third floor of a stunning Victorian mansion block. This property boasts high ceilings throughout, charming roof top views and porterage.",
+            "url": "http://www.zoopla.co.uk/for-sale/details/29862117",
+            "thumb": "http://li.zoocdn.com/712dd62a8792944cf42ca86f37922b558bfe2cf1_80_60.jpg"
+        },
+        {
+        	"id": 29858896,
+        	"transaction_type": 1,
+        	"property_placement": "Second Floor",
+        	"property_type": "Flat",
+        	"latitude": 51.504913,
+        	"longitude": -0.124878,
+        	"address": "Craven Street, Covent Garden, London",
+        	"postcode": "WC2N",
+        	"bedrooms": 1,
+        	"price": 765000,
+        	"pricetype": "In The Region Of",
+        	"summary": "This bright one bedroomed flat offers ample living space and boasts creative design, stylish décor and a superb location just off Trafalgar Square and moments from the Strand and the River Thames.",
+        	"url": "http://www.zoopla.co.uk/for-sale/details/29858896",
+        	"thumb": "http://li.zoocdn.com/39d7af8640f6badb5cc8ebb786db108c8c807163_80_60.jpg"
+        },
+        {
+        	"id": 29866953,
+        	"transaction_type": 1,
+        	"property_placement": "15th - 21st Floor",
+        	"property_type": "Duplex",
+        	"latitude": 51.503186,
+        	"longitude": -0.116215,
+        	"address": "Apartment C1801 Neo Bankside, Sumner Street, Southbank, London",
+        	"postcode": "SE1",
+        	"bedrooms": 3,
+        	"price": 53000000,
+        	"pricetype": "In The Region Of",
+        	"summary": "Multiple award-winning neo Bankside has released a premium selection of just 14 substantially larger 3 and 4 bedroom apartments with endless views of London's iconic skyline.",
+        	"url": "http://www.zoopla.co.uk/for-sale/details/29866953",
+        	"thumb": "http://li.zoocdn.com/5d3f5a28af8175a14f88a03c23e238e99ba160fc_80_60.jpg"
+        }
+    ]
+}
